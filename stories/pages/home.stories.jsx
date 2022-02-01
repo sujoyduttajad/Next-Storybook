a06816b4464a89eedaeb4b387e9e2d26bafd9702
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-02-01 21:24:30 +0530

    Update home.stories.jsx

--- stories/pages/home.stories.jsx
+++ stories/pages/home.stories.jsx
@@ -4,9 +4,16 @@ import Home from '../../pages/index'
 export default {
     title: "Pages/Home",
     component: Home,
+    // ------ Add Default args -----
+    args: {
+        name: "John Doe",
+    },
 };
 
 
-//  Add Controls to Next.js Page Props with Story Args 
+// ------ Add Controls to Next.js Page Props with Story Args -----
+// export const HomePage = (args) => <Home {...args} />
+// HomePage.args = { name: "John Damon" } 
+
+// ------ Add Default args -----
 export const HomePage = (args) => <Home {...args} />
-HomePage.args = { name: "John Damon" } 
