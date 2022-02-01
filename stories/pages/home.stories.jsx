bf06268eea73f774b6959094a3774367fdb4ffd9
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-02-01 21:00:52 +0530

    Update home.stories.jsx

--- stories/pages/home.stories.jsx
+++ stories/pages/home.stories.jsx
@@ -7,6 +7,6 @@ export default {
 };
 
 
-export const HomePage = () => <Home/>
-
-
+//  Add Controls to Next.js Page Props with Story Args 
+export const HomePage = (args) => <Home {...args} />
+HomePage.args = { name: "John Damon" } 
