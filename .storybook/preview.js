aa950b7bfcc41c880d7f9f2548c3f2454e71bc43
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-02-01 22:10:22 +0530

    Update preview.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,20 @@
 import "../styles/globals.css"
 // import everything from next/image
 import * as NextImage from "next/image";
+// Exports from msw
+import { setupWorker, rest } from "msw";
+
+// Quick check to make sure we are in a browser
+if( typeof global.process === "undefined" ) {
+  // We'll do an assignment for a new worker called setupWorker
+  const worker = setupWorker(
+    // Inside this function I wanna setup a rest route
+    rest.get("http://localhost:3000/api/hello", (req, res, ctx) => {
+      return res(ctx.json({ name: "John Doe" }))
+    })
+  );
+  worker.start();
+}
 
 const OriginalNextImage = NextImage.default;
 
